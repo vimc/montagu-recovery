6f7d8b98cc021a15dd540c5eb504f012dd0b97c7
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2017-12-04 17:08:04 +0000

    Most of the support restore working now

--- vagrant/run.sh
+++ vagrant/run.sh
@@ -2,8 +2,8 @@ sudo su
 mkdir /mnt/data/montagu
 ln -s /mnt/data/montagu /montagu
 
+## 1. Prepare the backup
 git clone https://github.com/vimc/montagu-backup /montagu/backup
-git clone https://github.com/vimc/montagu-vault  /montagu/vault
 
 mkdir -p /etc/montagu/backup
 cp /montagu/backup/configs/support.montagu/* /etc/montagu/backup/
@@ -13,15 +13,24 @@ pip3 install --quiet -r /montagu/backup/requirements.txt
 
 /montagu/backup/restore.py
 
-## Get the vault up
+## 2. Get the vault up
+git clone https://github.com/vimc/montagu-vault  /montagu/vault
 git -C /montagu/vault checkout i959
 (cd /montagu/vault && ./run-no-ssl.sh)
 
+## This goes twice
 /montagu/vault/unseal-loopback.sh
 /montagu/vault/unseal-loopback.sh
 
+## Restart vault
 (cd /montagu/vault && ./restart-with-ssl.sh)
 
+## Unlock a second time
 export VAULT_ADDR=https://support.montagu.dide.ic.ac.uk:8200
 vault unseal
 vault unseal
+
+## 3. Registry
+git clone https://github.com/vimc/montagu-registry  /montagu/registry
+pip3 install --user -r /montagu/registry/requirements.txt
+(cd /montagu/registry && ./montagu-registry start)
